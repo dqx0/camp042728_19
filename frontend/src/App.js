d72aaa60e575265e004411ad4faf8dfe4189bdbe
unknown <g2172049@tcu.ac.jp>
2024-04-28 11:25:51 +0900

    router OK

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,18 @@
 import './App.css';
-import React,{useState} from "react"
-
-export const UsingRegister=()=>{
-    const[use,setInputUse]=useState("");
-    const handleChange=(e)=>{
-        setInputUse(e.target.value);
-        console.log(use+":error")
-    };
-    const handleClick=(e)=>{
-        console.log("aaaa");
-        const body={
-            use:use
-        };
-        fetch("http://localhost:/3000/use",{
-            method:'POST',
-            body:JSON.stringify(body)
-        })
-        .then(response=>{
-            if(!response.ok){
-                throw new Error('response error');
-            }
-            console.log(body);
-            return response.json();
-        })
-        .then(data=>{
-            console.log(data);
-        })
-    }
-    return (
-        <div>
-            <p>金額</p>
-            <input onClick={handleClick} onChange={handleChange} />
-            <button onClick={handleClick}>送信</button>
-        </div>
-    );
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import UsingRegister from './UsingRegister'
+import ExpensesRegister from './ExpensesRegister';
+const App=()=>{
+    return(
+        <Router>
+            <div>
+                <Routes>
+                    <Route path="/UsingRegister" element={<UsingRegister />} />
+                    <Route path="/ExpensesRegister" element={<ExpensesRegister />}/>
+                </Routes>
+                
+            </div>
+        </Router>
+    )
 }
-export default UsingRegister;
+export default App;
