a0507d8e6f7f3484d57e8b302d31132e0083d28f
unknown <g2172049@tcu.ac.jp>
2024-04-28 12:48:08 +0900

    function change

--- frontend/src/UsingRegister.js
+++ frontend/src/UsingRegister.js
@@ -1,38 +1,17 @@
 import './App.css';
-import React,{useState} from "react"
+import React,{useState,useEffect} from "react"
 
 export const UsingRegister=()=>{
-    const[use,setInputUse]=useState("");
-    const handleChange=(e)=>{
-        setInputUse(e.target.value);
-        console.log(use+":error")
-    };
-    const handleClick=(e)=>{
-        console.log("aaaa");
-        const body={
-            use:use
-        };
-        fetch("http://localhost:/3000/use",{
-            method:'POST',
-            body:JSON.stringify(body)
+    const [data,setData]=useState('');
+    useEffect(()=>{
+        fetch('http://localhost:8080/api/expense/1',{method:"GET"})
+        .then((response)=>response.json)
+        .then((getData)=>{
+            setData(getData);
         })
-        .then(response=>{
-            if(!response.ok){
-                throw new Error('response error');
-            }
-            console.log(body);
-            return response.json();
-        })
-        .then(data=>{
-            console.log(data);
-        })
-    }
+    })
     return (
-        <div>
-            <p>金額</p>
-            <input onClick={handleClick} onChange={handleChange} />
-            <button onClick={handleClick}>送信</button>
-        </div>
+        <p>{data}</p>
     );
 }
 export default UsingRegister;
